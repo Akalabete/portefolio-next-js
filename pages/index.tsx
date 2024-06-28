942531de48473a5104398f9df51a183b7fd45fb1
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2024-06-28 19:49:58 +0200

    remove unproper param to meta datas

--- pages/index.tsx
+++ pages/index.tsx
@@ -123,7 +123,7 @@ export default function Home() {
         <meta property="og:url" content={`https://www.lacouralexandre.tech`} />
         <link rel="canonical" href={`https://www.lacouralexandre.tech`} />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Porte folio d'alexandre LACOUR" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
