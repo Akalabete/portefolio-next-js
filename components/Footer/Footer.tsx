6b3b076ab01ede10116fd65bed9a985bde75a5f1
Alexandre LACOUR <100684188+Akalabete@users.noreply.github.com>
2024-06-28 19:37:40 +0200

    Update Footer.tsx
    removed unused socials

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -13,14 +13,12 @@ const ClickableIcon = props => {
 const IconsData = [
   { href: "https://github.com/Akalabete", Icon: GithubIcon },
   { href: "https://www.linkedin.com/in/lacour-alexandre", Icon: LinkedinIcon },
-  { href: "https://www.instagram.com/titof_abdo/", Icon: InstagramIcon },
-  { href: "https://www.youtube.com/@abdellatif_anaflous", Icon: YoutubeIcon },
 ];
 
 export default function Footer(props: { githubUrl: string; hideSocialsInDesktop: boolean }) {
   return (
     <div className="bg-AAprimary flex flex-col justify-center items-center py-8 space-y-4">
-      {/* // ? Reach me at */}
+      {/* // ? Pour me suivre */}
       <div className={`flex flex-row space-x-8 ${props.hideSocialsInDesktop ? "lg:hidden" : ""}`}>
         {IconsData.map((iconData, index) => {
           return <ClickableIcon key={index} href={iconData.href} Icon={iconData.Icon} />;
